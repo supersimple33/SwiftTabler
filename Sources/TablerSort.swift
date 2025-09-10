85bfb65a4c69a8f7364bf36b6a00760831502efc
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-09-10 16:46:54 -0400

    Checking whether swift data is @available

--- Sources/TablerSort.swift
+++ Sources/TablerSort.swift
@@ -156,37 +156,69 @@ public extension View {
     func tablerSort<E: NSObject>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UUID?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
     
     /// Swift-data support
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Bool>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Bool?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Date>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Date?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Double>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Double?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Float>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Float?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int16>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int16?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int32>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int32?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int64>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int64?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int8>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int8?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, Int?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, String>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, String?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt16>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt16?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt32>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt32?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt64>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt64?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt8>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt8?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UInt?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UUID>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
+    @available(iOS 17.0, macCatalyst 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
     func tablerSort<E: PersistentModel>(_ c: Binding<TablerContext<E>>, _ k: KeyPath<E, UUID?>) -> SortDescriptor<E> { SortDescriptor<E>(k, order: xlat(updateSort(c, k))) }
 
     private func xlat<Element>(_ direction: TablerSort<Element>.Direction) -> SortOrder {
